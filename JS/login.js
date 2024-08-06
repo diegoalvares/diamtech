69e59380f299783da91b00b4693f15a6e4181b1e
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2024-08-06 19:54:19 -0300

    atualizando.

--- JS/login.js
+++ JS/login.js
@@ -3,7 +3,7 @@ user = document.getElementById('usuario')
 pass = document.getElementById('senha')
 
 login.addEventListener('click', function(){
-    if (user.value == 'josi' & pass.value == '1' || user.value == 'diego' & pass.value =='05101994'){
+    if (user.value.toUpperCase() == 'JOSI' & pass.value == '1' || user.value.toUpperCase() == 'DIEGO' & pass.value =='05101994'){
         window.location.assign('index.html')
     }
 
